6f7491c040e671d8b7bd550774aa4dec6e9cf177
Rob Stenson <rob.stenson@gmail.com>
2020-04-09 15:46:27 -0700

    messin around

--- test.py
+++ test.py
@@ -1,48 +1,21 @@
 from coldtype import *
-from coldtype.pens.translationpen import TranslationPen
-import defcon
+from coldtype.pens.translationpen import TranslationPen, polarCoord
+from math import radians
 
-img_path = Path("media/brrrrr.png").resolve()
-src_path = Path("src/chromatics.ufo").resolve()
-try:
-    src = defcon.Font(str(src_path))
-except:
-    src = defcon.Font()
-    src.insertGlyph(DATPen().oval(Rect(500, 500)).to_glyph(), "A")
+src = DefconFont("src/chromatics.ufo")
 
-#src.images[img_path.name] = img_path.read_bytes()
-#img = defcon.Image(imageDict=dict(fileName=img_path.name))
-#src["A"].image = img
-#src.save(str(src_path))
+def Glyphs(ufo, glyphNames):
+    return DATPenSet([DATPen().glyph(ufo[gn]) for gn in glyphNames]).distribute()
 
-def project(frontAngle, frontWidth):
-    d = frontWidth
-    lightAngle = frontAngle
-    lightZAngle = 45; # how "straight-on" the light is (?)
-    lightAngleRad = lightAngle * math.pi/180 - math.pi/2;
-    lightZAngleRad = lightZAngle * math.pi/180;
-    xTranslate = math.tan(lightZAngleRad) * d * math.cos(lightAngleRad);
-    yTranslate = math.tan(lightZAngleRad) * d * math.sin(lightAngleRad);
-    return (xTranslate, yTranslate)
+DATPenSet.Glyphs = Glyphs
 
-def castshadow(self, angle=-45, width=100, ro=1):
-    out = DATPen()
-    tp = TranslationPen(out, frontAngle=angle, frontWidth=width)
-    self.replay(tp)
-    out.record(self.copy().translate(*project(-angle, width)))
-    if ro:
-        out.removeOverlap()
-    self.value = out.value
-    return self
-
-DATPen.castshadow = castshadow
-
-@renderable()
+@renderable(bg=1, watch=[src.path])
 def test(r):
-    A = DATPen().glyph(src["A"]).removeOverlap()
+    #A = StyledString("AR", Style(font, 1000, t=10)).pen().align(r).removeOverlap()
+    #A = DATPen().glyph(src["A"]).align(r)
+    A = DATPenSet.Glyphs(src, ["R","A","I"]).pen()
     A_comp = DATPenSet()
-    A_comp += A.copy().castshadow().f("hr", 0.5, 0.5)#.s(0).sw(3)
-    A_comp += A.copy().castshadow(width=20).f(1)
-    #A_comp += A.copy().outline(10).f(1)
-    A_comp += A.f("hr", 0.5, 0.5)
-    return A_comp.align(r)
+    knock = A.copy().grow(10).project(-45, 5).f(1)#.s(0)
+    A_comp += A.copy().castshadow(-45, 100).difference(knock).f(Gradient.Vertical(r, ("hr",0.5,0.35), ("hr",0.5,0.35)))
+    A_comp += A.f(Gradient.Horizontal(r, ("hr", 0.65, 0.5), ("hr", 0.65, 0.5)))
+    return A_comp
